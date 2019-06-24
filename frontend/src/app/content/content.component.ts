b3488ca67cef623c1a97eedd75b053935eb5e80c
jonas.fabian <jonas.fabian@fhnw.ch>
2019-06-24 08:52:45 +0200

    upload text file now possible

--- frontend/src/app/content/content.component.ts
+++ frontend/src/app/content/content.component.ts
@@ -10,7 +10,18 @@ export class ContentComponent implements OnInit {
   constructor() {
   }
 
+  file: any;
+  text: string | ArrayBuffer;
+
   ngOnInit() {
   }
 
+  fileChanged(e) {
+    const reader = new FileReader();
+    this.file = e.target.files[0];
+    reader.onload = () => {
+      this.text = reader.result;
+    };
+    reader.readAsText(this.file);
+  }
 }
