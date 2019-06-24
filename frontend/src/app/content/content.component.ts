64c5e8a47e44400ea94de3bed92fe62471731b88
jonas.fabian <jonas.fabian@fhnw.ch>
2019-06-24 10:29:23 +0200

    added waveSurfer library and added dummy-song

--- frontend/src/app/content/content.component.ts
+++ frontend/src/app/content/content.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import WaveSurfer from 'wavesurfer.js';
+import TimelinePlugin from 'wavesurfer.js/dist/plugin/wavesurfer.timeline.min.js';
+import CursorPlugin from 'wavesurfer.js/dist/plugin/wavesurfer.cursor.min.js';
+import RegionsPlugin from 'wavesurfer.js/dist/plugin/wavesurfer.regions.js';
 
 @Component({
   selector: 'app-content',
@@ -7,13 +11,53 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ContentComponent implements OnInit {
 
-  constructor() {
+  constructor(
+  ) {
   }
 
   file: any;
   text: string | ArrayBuffer;
+  waveSurfer: WaveSurfer = null;
 
   ngOnInit() {
+    this.onPreviewPressed();
+  }
+
+  generateWaveform(): void {
+    Promise.resolve(null).then(() => {
+      this.waveSurfer = WaveSurfer.create({
+        container: '#waveform',
+        waveColor: 'lightblue',
+        progressColor: 'blue',
+        responsive: true,
+        plugins: [
+          TimelinePlugin.create({
+            container: '#wave-timeline'
+          }),
+          CursorPlugin.create({
+            container: '#wave-cursor',
+            showTime: true,
+            opacity: 1
+          }),
+          RegionsPlugin.create({
+            regions: [
+              {
+                start: 1,
+                end: 3,
+                color: 'hsla(200, 50%, 70%, 0.4)'
+              }
+            ]
+          })
+        ]
+      });
+    });
+  }
+
+  onPreviewPressed(): void {
+    if (!this.waveSurfer) {
+      this.generateWaveform();
+    }
+    Promise.resolve().then(() => this.waveSurfer.load('../assets/song.mp3'));
   }
 
   fileChanged(e) {
