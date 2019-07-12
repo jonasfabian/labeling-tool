2dd4feffd70311d4d687646a2312671d5ea8556f
jonas.fabian <jonas.fabian@fhnw.ch>
2019-07-12 14:13:57 +0200

    -added sidenav -added routing -removed tabs

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
+import {ContentComponent} from './content/content.component';
+import {MatchOverviewComponent} from './match-overview/match-overview.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'label',
+    pathMatch: 'full'
+  },
+  {
+    path: 'label',
+    component: ContentComponent
+  },
+  {
+    path: 'overview',
+    component: MatchOverviewComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/navigation-menu/navigation-menu.component.ts
+++ frontend/src/app/navigation-menu/navigation-menu.component.ts
@@ -12,5 +12,4 @@ export class NavigationMenuComponent implements OnInit {
 
   ngOnInit() {
   }
-
 }
