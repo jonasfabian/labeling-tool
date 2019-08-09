a22d57d0c25471cd451cab98c3da402cd7600919
jonas.fabian <jonas.fabian@fhnw.ch>
2019-08-09 15:37:08 +0200

    fix login without hardcoded values

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ApiService} from '../../services/api.service';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Router} from '@angular/router';
-import {User} from '../../models/user';
-import {EmailPassword} from "../../models/EmailPassword";
+import {EmailPassword} from '../../models/EmailPassword';
 
 @Component({
   selector: 'app-login',
@@ -16,43 +15,32 @@ export class LoginComponent implements OnInit {
     private fb: FormBuilder,
     private apiService: ApiService,
     private router: Router
-  ) { }
+  ) {
+  }
 
-  registerForm: FormGroup;
-  user: User = new User(-1, '', '', '', '');
+  loginForm: FormGroup;
 
   ngOnInit() {
     this.initForm();
   }
 
   initForm(): void {
-    this.registerForm = this.fb.group({
+    this.loginForm = this.fb.group({
       email: ['', [Validators.required]],
       password: ['', [Validators.required]]
     });
   }
 
-  register(): void {
-    this.user.email = this.registerForm.controls.email.value;
-    this.user.password = this.registerForm.controls.password.value;
-    if (this.registerForm.valid) {
-      this.apiService.createUser(this.user).subscribe(_ => {
+  login(): void {
+    if (this.loginForm.valid) {
+      this.apiService.checkLogin(
+        new EmailPassword(this.loginForm.controls.email.value, this.loginForm.controls.password.value)).subscribe(_ => {
         this.router.navigate(['home']);
+      }, error => {
+        if (error.status === 401) {
+          alert('Unauthorized');
+        }
       });
     }
-  }
-
-  cancel(): void {
-    this.registerForm.reset();
-  }
-
-  check(): void {
-    this.apiService.checkLogin(new EmailPassword('j.fabian@gmx.ch', 'yeet')).subscribe(_ => {
-      this.router.navigate(['home']);
-    }, error => {
-      if (error.status === 401) {
-        alert('Unauthorized');
-      }
-    });
   }
 }
