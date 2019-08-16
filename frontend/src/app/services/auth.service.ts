b7be6d53ca99f5e40bd84bdc85fa80e82b4e0d05
jonas.fabian <jonas.fabian@fhnw.ch>
2019-08-16 11:03:23 +0200

    fix logout

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,12 +1,15 @@
 import {Injectable} from '@angular/core';
 import {UserPublicInfo} from '../models/UserPublicInfo';
+import {Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() {
+  constructor(
+    private router: Router
+  ) {
   }
 
   isAuthenticated = false;
@@ -15,9 +18,11 @@ export class AuthService {
   checkAuthenticated(): void {
     if (sessionStorage.getItem('user')) {
       JSON.parse(sessionStorage.getItem('user')).map(r => {
-        const expirationDate = new Date(new Date(r.time).setMinutes(new Date(r.time).getMinutes() + 30));
+        const expirationDate = new Date(new Date(r.time).setMinutes(new Date(r.time).getMinutes() + 1));
         if (expirationDate < new Date()) {
+          sessionStorage.clear();
           this.isAuthenticated = false;
+          location.reload();
         }
         this.loggedInUser = new UserPublicInfo(r.id, r.firstName, r.lastName, r.email);
       });
