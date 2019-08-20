96846b310423ab6d4b716e3c9072269730f1ebd8
jonas.fabian <jonas.fabian@fhnw.ch>
2019-08-20 13:19:45 +0200

    fix when no data available

--- frontend/src/app/components/session-overview/session-overview.component.ts
+++ frontend/src/app/components/session-overview/session-overview.component.ts
@@ -8,18 +8,23 @@ import {Component, OnInit} from '@angular/core';
 
 export class SessionOverviewComponent implements OnInit {
 
-  constructor(
-  ) {
+  constructor() {
   }
 
   showLegend = true;
   single = [];
+  noDataAvailable = true;
   colorScheme = {
     domain: ['#3f51b5', '#7482cf', 'blue', 'darkblue']
   };
 
   ngOnInit() {
     const sessionData = JSON.parse(sessionStorage.getItem('checkData'))[0];
+    if (sessionData.correct === 0 && sessionData.wrong === 0 && sessionData.skipped === 0) {
+      this.noDataAvailable = true;
+    } else {
+      this.noDataAvailable = false;
+    }
     this.single = [
       {name: 'Correct', value: sessionData.correct},
       {name: 'Wrong', value: sessionData.wrong},
