9aaab03047bdec5a81d358443484f46301d34f1a
jonas.fabian <jonas.fabian@fhnw.ch>
2019-09-17 13:25:40 +0200

    fix size of tiles in overview

--- frontend/src/app/components/graphs/number-cards/number-cards.component.ts
+++ frontend/src/app/components/graphs/number-cards/number-cards.component.ts
@@ -20,17 +20,22 @@ export class NumberCardsComponent implements OnInit {
   };
 
   ngOnInit() {
-    this.view = [innerWidth / 4, innerHeight / 6];
+    this.view = [innerWidth / 1.6, innerHeight / 2.2];
     this.apiService.getLabeledSums().subscribe(l => l.forEach(s => {
       this.single = [
         {name: 'Not-Labeled', value: s.nonLabeled},
         {name: 'Correct', value: s.correct},
         {name: 'Wrong', value: s.wrong},
-        {name: 'Skipped', value: s.skipped}];
+        {name: 'Not-Labeled', value: s.nonLabeled},
+        {name: 'Correct', value: s.correct},
+        {name: 'Wrong', value: s.wrong},
+        {name: 'Not-Labeled', value: s.nonLabeled},
+        {name: 'Correct', value: s.correct}
+      ];
     }));
   }
 
   onResize(event) {
-    this.view = [event.target.innerWidth / 4, event.target.innerHeight / 6];
+    this.view = [event.target.innerWidth / 1.6, event.target.innerHeight / 2.2];
   }
 }
