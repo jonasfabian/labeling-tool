4f821e965fad8eb411aaea6d59fd44712da513f0
jonasfabian <jonas.fabian@fhnw.ch>
2019-12-19 15:09:12 +0100

    better snake

--- frontend/src/app/components/snake/snake.component.ts
+++ frontend/src/app/components/snake/snake.component.ts
@@ -21,8 +21,9 @@ export class SnakeComponent implements OnInit {
   xCoord = 0;
   currentDirection = '';
   cellArray: Array<Point> = [];
+  snakeArray: Array<Point> = [];
   randomItemArray: Array<Point> = [];
-  maxLength = 3;
+  maxLength = 1;
   interval: any;
   score = 0;
   lost = false;
@@ -38,13 +39,21 @@ export class SnakeComponent implements OnInit {
   @HostListener('window:keydown', ['$event'])
   keyEvent(event: KeyboardEvent) {
     if (event.key === 'ArrowLeft') {
-      this.currentDirection = 'left';
+      if (this.currentDirection !== 'right') {
+        this.currentDirection = 'left';
+      }
     } else if (event.key === 'ArrowRight') {
-      this.currentDirection = 'right';
+      if (this.currentDirection !== 'left') {
+        this.currentDirection = 'right';
+      }
     } else if (event.key === 'ArrowUp') {
-      this.currentDirection = 'up';
+      if (this.currentDirection !== 'down') {
+        this.currentDirection = 'up';
+      }
     } else if (event.key === 'ArrowDown') {
-      this.currentDirection = 'down';
+      if (this.currentDirection !== 'up') {
+        this.currentDirection = 'down';
+      }
     }
   }
 
@@ -57,55 +66,79 @@ export class SnakeComponent implements OnInit {
     this.ctx.strokeRect(x, y, this.cellSize, this.cellSize);
   }
 
-  play(): void {
-    this.interval = setInterval(() => {
-      const randomItem = this.generateRandomItem();
-      if (this.randomItemArray.length === 0) {
-        this.drawCell(randomItem, 'green');
-        this.randomItemArray.push(randomItem);
-      } else {
-        this.randomItemArray.forEach(rItem => {
-          this.drawCell(rItem, 'green');
-        });
-      }
-      if (this.cellArray.length === 0) {
-        const firstCell = this.generateRandomItem();
-        this.drawCell(firstCell, 'blue');
-        this.cellArray.push(firstCell);
-      } else {
-        const headX = this.cellArray[this.cellArray.length - 1].x * this.cellSize;
-        const headY = this.cellArray[this.cellArray.length - 1].y * this.cellSize;
-        if (this.checkOwnCollision(this.cellArray) && this.cellArray.length > 4) {
-          this.clear();
+  fill(): void {
+    const numberX = this.ctx.canvas.width / this.cellSize;
+    const numberY = this.ctx.canvas.height / this.cellSize;
+    const numberOfCells = numberX * numberY;
+    let index = 0;
+    let x = 0;
+    let y = 0;
+    while (index < numberOfCells + ((this.cellSize / 2) - 1)) {
+      if (y * this.cellSize < this.ctx.canvas.height) {
+        if (x * this.cellSize < this.ctx.canvas.width) {
+          this.drawCell(new Point(x, y), 'whitesmoke');
+          this.cellArray.push(new Point(x, y));
+          x++;
+          index++;
+        } else {
+          y++;
+          x = 0;
+          index++;
         }
-        if (headX < this.ctx.canvas.width && headX >= 0 && headY < this.ctx.canvas.height && headY >= 0) {
-          this.cellArray.forEach(cell => {
-            this.drawCell(cell, 'blue');
+      }
+    }
+  }
+
+  play(): void {
+    this.fill();
+    this.interval = setInterval(() => {
+      if (this.randomItemArray.length === 0) {
+        const randomItem = this.generateRandomItem();
+        this.randomItemArray.push(randomItem);
+        this.drawCell(randomItem, 'green');
+      }
+      if (this.snakeArray.length === 0) {
+        const firstCell = this.generateRandomItem();
+        this.xCoord = firstCell.x;
+        this.yCoord = firstCell.y;
+        this.snakeArray.push(firstCell);
+        this.cellArray.splice(0, 1);
+      } else {
+        const tempArray: Array<Point> = [];
+        this.snakeArray.forEach(cell => {
+          tempArray.push(cell);
+          this.drawCell(cell, 'blue');
+        });
+        if ((new Set(tempArray)).size !== tempArray.length) {
+          clearInterval(this.interval);
+          this.apiService.createScore(this.authService.loggedInUser.getValue().id, this.score).subscribe(() => {
+          }, () => {
+          }, () => {
+            this.xCoord = 0;
+            this.yCoord = 0;
+            this.score = 0;
+            this.lost = true;
+            this.snakeArray = [];
+            this.cellArray = [];
+            this.randomItemArray = [];
+            this.audioPlayer.nativeElement.src = '../../../../assets/oof.mp3';
+            this.audioPlayer.nativeElement.play();
           });
-          if (this.currentDirection === 'left') {
-            this.xCoord--;
-          } else if (this.currentDirection === 'right') {
-            this.xCoord++;
-          } else if (this.currentDirection === 'up') {
-            this.yCoord--;
-          } else if (this.currentDirection === 'down') {
-            this.yCoord++;
-          }
-          this.cellArray.push(new Point(false, this.xCoord, this.yCoord));
-          if (((this.cellArray[this.cellArray.length - 1].x * this.cellSize) === this.randomItemArray[0].x * this.cellSize) && ((this.cellArray[this.cellArray.length - 1].y * this.cellSize) === this.randomItemArray[0].y * this.cellSize)) {
+        }
+        this.snakeArray = tempArray;
+        const headX = this.snakeArray[this.snakeArray.length - 1].x * this.cellSize;
+        const headY = this.snakeArray[this.snakeArray.length - 1].y * this.cellSize;
+        if (headX < this.ctx.canvas.width && headX >= 0 && headY < this.ctx.canvas.height && headY >= 0) {
+          if (this.snakeArray[this.snakeArray.length - 1].x === this.randomItemArray[0].x && this.snakeArray[this.snakeArray.length - 1].y === this.randomItemArray[0].y) {
             this.score = this.score + 10;
-            this.randomItemArray.splice(0, 1);
             this.maxLength++;
-            let newRandom = this.generateRandomItem();
-            if (this.cellArray.includes(newRandom)) {
-              newRandom = this.generateRandomItem();
-            }
-            this.drawCell(newRandom, 'green');
-            this.randomItemArray.push(newRandom);
-          }
-          if (this.cellArray.length > this.maxLength) {
-            this.drawCell(this.cellArray[0], 'white');
-            this.cellArray.splice(0, 1);
+            this.cellArray.splice(this.cellArray.indexOf(this.randomItemArray[0]), 1);
+            this.randomItemArray[0] = this.generateRandomItem();
+            this.drawCell(this.randomItemArray[0], 'green');
+          } else {
+            this.drawCell(this.snakeArray[0], 'whitesmoke');
+            this.snakeArray.splice(0, 1);
+            this.cellArray.push(this.snakeArray[0]);
           }
         } else {
           clearInterval(this.interval);
@@ -116,11 +149,25 @@ export class SnakeComponent implements OnInit {
             this.yCoord = 0;
             this.score = 0;
             this.lost = true;
+            this.snakeArray = [];
+            this.cellArray = [];
+            this.randomItemArray = [];
             this.audioPlayer.nativeElement.src = '../../../../assets/oof.mp3';
             this.audioPlayer.nativeElement.play();
           });
         }
       }
+      if (this.currentDirection === 'left') {
+        this.xCoord--;
+      } else if (this.currentDirection === 'right') {
+        this.xCoord++;
+      } else if (this.currentDirection === 'up') {
+        this.yCoord--;
+      } else if (this.currentDirection === 'down') {
+        this.yCoord++;
+      }
+      this.snakeArray.push(new Point(this.xCoord, this.yCoord));
+      this.cellArray.splice(this.cellArray.indexOf(new Point(this.xCoord, this.yCoord)), 1);
     }, 200);
   }
 
@@ -129,33 +176,34 @@ export class SnakeComponent implements OnInit {
     return y - (y % (precision === undefined ? 1 : +precision));
   }
 
-  checkOwnCollision(array: Array<Point>): boolean {
+  selfHit(array: Array<Point>): boolean {
     return (new Set(array)).size !== array.length;
   }
 
   generateRandomItem(): Point {
-    const min = Math.ceil(0);
-    const max = Math.floor(this.ctx.canvas.width);
-    return new Point(true, this.roundToPrecision(Math.floor(Math.random() * (max - min + 1)) + min / this.cellSize, this.cellSize) /
-      this.cellSize, this.roundToPrecision(Math.floor(Math.random() * (max - min + 1)) + min / this.cellSize, this.cellSize) / this.cellSize);
+    const min = 0;
+    const max = this.cellArray.length;
+    const rdmNumber = this.roundToPrecision(Math.random() * (max - min) + min, 1);
+    return this.cellArray[rdmNumber];
   }
 
   clear(): void {
     clearInterval(this.interval);
     this.lost = false;
+    this.score = 0;
     this.cellArray = [];
+    this.snakeArray = [];
     this.randomItemArray = [];
+    this.currentDirection = '';
     this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
   }
 }
 
 export class Point {
-  isItem: boolean;
   x: number;
   y: number;
 
-  constructor(isItem: boolean, x: number, y: number) {
-    this.isItem = isItem;
+  constructor(x: number, y: number) {
     this.x = x;
     this.y = y;
   }
