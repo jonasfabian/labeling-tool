bdb385533b16b40e950438fb3d0d73ede49f1cd1
Elias Schorr <elias.schorr@fhnw.ch>
2020-02-21 16:07:31 +0100

    fix bugs

--- backend/src/main/java/ch/fhnw/labeling_tool/user/CustomUserDetailsService.java
+++ backend/src/main/java/ch/fhnw/labeling_tool/user/CustomUserDetailsService.java
@@ -85,9 +85,6 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     public void putPassword(ChangePassword changePassword) {
         User user = userDao.fetchOneById(getLoggedInUserId());
-        System.out.println("putPassword");
-        System.out.println(changePassword.getPassword());
-        System.out.println(changePassword.getNewPassword());
         if (passwordEncoder.matches(changePassword.getPassword(), user.getPassword())) {
             user.setPassword(passwordEncoder.encode(changePassword.getNewPassword()));
             userDao.update(user);
@@ -98,6 +95,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     public void register(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setEnabled(true);
         userDao.insert(user);
         Long id = userDao.fetchOneByUsername(user.getUsername()).getId();
         //    TODO not sure how to handle public logins for now we just add them to the public group
