bd913f0c49484bd94244672723a15f3465ebea1a
Elias Schorr <elias.schorr@fhnw.ch>
2020-03-03 12:36:37 +0100

    refactor overview.component

--- frontend/src/app/components/admin/overview/overview.component.ts
+++ frontend/src/app/components/admin/overview/overview.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit, ViewChild} from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
@@ -21,28 +21,21 @@ export interface OverviewOccurrence {
   styleUrls: ['./overview.component.scss']
 })
 export class OverviewComponent implements OnInit {
-  // TODO simplify component
-  @ViewChild('textAreaText') textAreaText: ElementRef<HTMLTextAreaElement>;
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
   showTextAudio = true;
-  // TODO change dao
   dataSource = new MatTableDataSource<OverviewOccurrence | { id: number, text: string, username: string, time: string }>();
   columns = ['text', 'correct', 'wrong', 'options'];
   private selectedTextAudioDto: OverviewOccurrence;
-  // todo switch -> probably only show recordings for others.
-  private mode: OccurrenceMode = OccurrenceMode.TEXT_AUDIO;
 
   constructor(private det: ChangeDetectorRef, private httpClient: HttpClient, private userGroupService: UserGroupService) {
   }
 
   ngOnInit() {
-    // TODO not sure we even need a datasource instead of an array?
-    this.getTextAudios().subscribe(textAudio => {
-      this.dataSource = new MatTableDataSource<OverviewOccurrence>(textAudio);
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
-    });
+    this.dataSource = new MatTableDataSource<OverviewOccurrence>([]);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+    this.reload();
   }
 
   selectElement(textAudio: OverviewOccurrence) {
@@ -51,22 +44,25 @@ export class OverviewComponent implements OnInit {
 
   toggleChangeView(): void {
     this.showTextAudio = !this.showTextAudio;
-    // TODO simplify like check component
+    this.reload();
+  }
+
+  reload() {
     this.getTextAudios().subscribe(textAudio => {
-      this.dataSource = new MatTableDataSource<OverviewOccurrence>(textAudio);
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
+      this.dataSource.data = textAudio;
     });
   }
 
   edit(element: any) {
+    // TODO implement logic
   }
 
   play(element: any) {
+    // TODO implement logic
   }
 
-  // TODO only return the top 100 audio filtered by label status
   private getTextAudios(): Observable<Array<OverviewOccurrence>> {
-    return this.httpClient.get<Array<OverviewOccurrence>>(`${environment.url}user_group/${this.userGroupService.userGroupId}/admin/overview_occurrence?mode=${(this.mode)}`);
+    const mode = this.showTextAudio ? OccurrenceMode.TEXT_AUDIO : OccurrenceMode.RECORDING;
+    return this.httpClient.get<Array<OverviewOccurrence>>(`${environment.url}user_group/${this.userGroupService.userGroupId}/admin/overview_occurrence?mode=${(mode)}`);
   }
 }
